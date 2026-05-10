0b077113ad07d897659b23bb9e09d095d4178ffa
Aaron Jomy <aaronjomyjoseph@gmail.com>
2026-05-10 11:00:31 +0200

    Re-enable numba tests

--- test/test_numba.py
+++ test/test_numba.py
@@ -117,7 +117,6 @@ class TestNUMBA:
         assert (go_fast(x) == go_slow(x)).all()
         assert self.compare(go_slow, go_fast, 300000, x)
 
-    @mark.xfail
     def test02_JITed_template_free_func(self):
         """Numba-JITing of Cling-JITed templated free function"""
 
@@ -151,7 +150,6 @@ class TestNUMBA:
         assert (go_fast(x) == go_slow(x)).all()
         assert self.compare(go_slow, go_fast, 100000, x)
 
-    @mark.xfail
     def test03_proxy_argument_for_field(self):
         """Numba-JITing of a free function taking a proxy argument for field access"""
 
@@ -185,7 +183,6 @@ class TestNUMBA:
         assert((go_fast(x, d) == go_slow(x, d)).all())
         assert self.compare(go_slow, go_fast, 10000, x, d)
 
-    @mark.xfail
     def test04_proxy_argument_for_method(self):
         """Numba-JITing of a free function taking a proxy argument for method access"""
 
@@ -270,7 +267,6 @@ class TestNUMBA:
 
         assert sum == tma(x)
 
-    @mark.xfail
     def test07_datatype_mapping(self):
         """Numba-JITing of various data types"""
 
@@ -304,7 +300,6 @@ class TestNUMBA:
                 val = getattr(nl[ntype], m)()
                 assert access_field(getattr(ns, 'M%d'%i)(val)) == val
 
-    @mark.xfail
     def test08_object_returns(self):
         """Numba-JITing of a function that returns an object"""
 
@@ -728,7 +723,6 @@ class TestNUMBA_DOC:
         import cppyy
         import cppyy.numba_ext
 
-    @mark.xfail
     def test01_templated_freefunction(self):
         """Numba support documentation example: free templated function"""
 
@@ -757,7 +751,6 @@ class TestNUMBA_DOC:
         assert type(tsa(a)) == int
         assert tsa(a) == 285
 
-    @mark.xfail
     def test02_class_features(self):
         """Numba support documentation example: class features"""
 
